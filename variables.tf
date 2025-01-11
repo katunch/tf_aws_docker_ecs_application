d1354b23d6355dae302f407e9119c5bac7541990
Silas Knobel <silas.knobel@katun.ch>
2025-01-11 15:46:36 +0100

    choose weather to enable or disable sidecar

--- variables.tf
+++ variables.tf
@@ -105,6 +105,12 @@ variable "image" {
   description = "The Docker image"
 }
 
+variable "sidecar_enabled" {
+  type        = bool
+  description = "Whether to enable the sidecar proxy"
+  default     = true
+}
+
 variable "sidecar_proxy_image" {
   type        = string
   description = "The sidecar proxy image"
