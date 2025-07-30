2fa4deda4c705cc53af4749221b8c595e16b1ed1
Silas Knobel <silas.knobel@katun.ch>
2025-07-30 11:37:18 +0200

    version bump

--- variables.tf
+++ variables.tf
@@ -140,7 +140,7 @@ variable "sidecar_enabled" {
 variable "sidecar_proxy_image" {
   type        = string
   description = "The sidecar proxy image"
-  default     = "ghcr.io/katunch/tf_aws_docker_ecs_application:v1.4.2"
+  default     = "ghcr.io/katunch/tf_aws_docker_ecs_application:v1.5.0"
 }
 
 variable "environment_variables" {
